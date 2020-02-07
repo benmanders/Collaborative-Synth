70f1e22738af9ad9adf9b3e04d1505741f583e85
benmanders <57005806+benmanders@users.noreply.github.com>
2020-02-07 09:28:06 +0000

    Random note sequence

--- Collaborative_Synthesiser/Collaborative_Synthesiser.ino
+++ Collaborative_Synthesiser/Collaborative_Synthesiser.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_VS1053.h>
 //#define VS1053_BANK_DRUMS1 0x78 // DRUM PATCH
 #define VS1053_BANK_DEFAULT 0x00 // DEFAULT MELODY PATCH
-#define VS1053_GM1_ACOUSTIC_GRAND_PIANO 49 // ACOUSTIC GRAND 
+#define VS1053_GM1_ACOUSTIC_GRAND_PIANO 1 // ACOUSTIC GRAND 
 #define MIDI_NOTE_ON  0x90
 #define MIDI_NOTE_OFF 0x80
 #define MIDI_CHAN_MSG 0xB0
@@ -12,14 +12,21 @@
 
 int lightSensorVal = 0;
 int potentiometerVal = 0;
-const int numBeats = 32;
+const int numBeats = 16;
 uint8_t beats[numBeats];
 uint8_t curBeat = 0;
-uint8_t numNotes = 15;
+const uint8_t numNotes = 16;
+bool noteMatrix[numBeats][numNotes];
+bool finalPattern[numBeats][numNotes];
+bool finalPatternNotes[3] = {3, 7, 4};
+bool finalPatternBeats[3] = {1, 4, 1};
+
+int pentatonicMidi[numNotes] {48, 50, 52, 55, 57, 60, 62, 64, 67, 69, 72, 74, 76, 79, 81, 84}; // cmaj pentatonic - 3 octactaves
 
 void setup()
 {
   Serial.begin(115200);
+  while (!Serial) {}
   Serial.println("VS1053 MIDI test");
   VS1053_MIDI.begin(31250); // MIDI uses a 'strange baud rate'
   midiSetChannelBank(0, VS1053_BANK_DEFAULT);
@@ -30,12 +37,19 @@ void setup()
   int potentiometerVal = analogRead(A2);
   setupBeats();
   // playNotes();
+  //  randomiseNoteMatrix();
+  //  setAscendingScale();
+  //setInterleaveScale();
+  setMatrixDensity(1);
+  printMatrix();
+
 }
 
 void loop()
 {
+  //  playChord();
   //readSensors();
-  //playNotes();
+  //  playNotes();
   //setupBeats();
   playStep();
 }
--- Collaborative_Synthesiser/sequencer_functions.ino
+++ Collaborative_Synthesiser/sequencer_functions.ino
@@ -1,51 +1,166 @@
 void playStep()
 {
-  if (beats[curBeat] != 0)
+
+  for (int note = 0; note < numNotes; note++)
   {
-    midiNoteOn(0, beats[curBeat], 127);
+    if (noteMatrix[curBeat][note])
+    {
+      midiNoteOn(0, pentatonicMidi[note], 127);
+    }
   }
-  delay(100);
-  midiNoteOff(0, beats[curBeat], 127);
+
+  delay(170);
+  for (int note = 0; note < numNotes; note++)
+  {
+    if (noteMatrix[curBeat][note] != 0)
+    {
+      midiNoteOff(0, pentatonicMidi[note], 127);
+    }
+  }
   curBeat++;
   curBeat %= numBeats;
 }
 
 void setupBeats()
 {
-  for (int n = 0; n < numNotes; n++)
-  {
-    int pentatonicMidi[16] {48, 50, 52, 55, 57, 60, 62, 64, 67, 69, 72, 74, 76, 79, 81, 84};
-    int noteIndex = map(analogRead(A1), 0, 1023, 0, 15);
-    Serial.println(noteIndex);
-    pentatonicMidi[noteIndex];
-  }
-
+  int noteIndex = map(analogRead(A1), 0, 1023, 0, 15);
+  Serial.println(noteIndex);
   {
     for (int i = 0; i < numBeats; i++)
     {
       beats[i] = 0;
     }
-    beats[0] = pentatonicMidi[noteIndex];
-    beats[4] = pentatonicMidi[noteIndex];
-    beats[10] = pentatonicMidi[noteIndex];
-    beats[14] = pentatonicMidi[noteIndex];
+    beats[0] = 48;
+    beats[8] = pentatonicMidi[noteIndex];
     beats[20] = pentatonicMidi[noteIndex];
-    beats[25] = pentatonicMidi[noteIndex];
     beats[28] = pentatonicMidi[noteIndex];
-    //beats[30] = 52;
+    beats[40] = pentatonicMidi[noteIndex];
+    beats[50] = 48;
+    beats[56] = pentatonicMidi[noteIndex];
+    beats[60] = 52;
+
+
   }
 }
 
-//void playNotes()
-//{
-//  for (int n = 0; n < numNotes; n++)
+void playChord()
+{
+  for (int n = 0; n < numNotes; n += 2)
+  {
+    midiNoteOn(0, pentatonicMidi[n], 127);
+  }
+  delay(200);
+  for (int n = 0; n < numNotes; n++)
+  {
+    midiNoteOff(0, pentatonicMidi[n], 127);
+  }
+}
+
+void playNotes()
+{
+
+  for (int n = 0; n < numNotes; n++)
+  {
+    int noteIndex = map(analogRead(A1), 0, 1023, 0, 15);
+    Serial.println(noteIndex);
+    pentatonicMidi[noteIndex];
+    midiNoteOn(0, pentatonicMidi[noteIndex], 127);
+    delay(200);
+    midiNoteOff(0, pentatonicMidi[noteIndex], 127);
+  }
+}
+
+void randomiseNoteMatrix()
+{
+  for (int b = 0; b < numBeats; b++)
+  {
+    for (int n = 0; n < numNotes; n++)
+    {
+      noteMatrix[b][n] = !(random(20) < 17);
+    }
+  }
+
+}
+
+
+void setAscendingScale()
+{
+  for (int b = 0; b < numBeats; b++)
+  {
+    for (int n = 0; n < numNotes; n++)
+    {
+      noteMatrix[b][n] = (b == n) ;
+    }
+  }
+}
+
+
+void setInterleaveScale()
+{
+  for (int b = 0; b < numBeats; b++)
+  {
+    for (int n = 0; n < numNotes; n++)
+    {
+      if ((b == n))
+      {
+        noteMatrix[b][n] = true;
+      }
+      else if ((b == numBeats - n) )
+      {
+        noteMatrix[b][n] = true;
+      }
+      else
+      {
+        noteMatrix[b][n] = false;
+      }
+    }
+  }
+}
+
+void printMatrix()
+{
+  for (int b = 0; b < numBeats; b++)
+  {
+    for (int n = 0; n < numNotes; n++)
+    {
+      Serial.print(noteMatrix[b][n]);
+      Serial.print('\t');
+    }
+    Serial.println();
+  }
+}
+
+void setMatrixFromSensor()
+{
+  int reading = map(0, 0 , 1023, 0, 3);
+  for (int i = 0; i < reading ; i++)
+  {
+    noteMatrix[finalPatternBeats[i]][finalPatternNotes[i]] = true;
+  }
+
+  for (int i = reading; i < 3 ; i++)
+  {
+    noteMatrix[finalPatternBeats[i]][finalPatternNotes[i]] = false;
+  }
+
+//  for (int b = 0; b < numBeats; b++)
 //  {
-//    int pentatonicMidi[16] {48, 50, 52, 55, 57, 60, 62, 64, 67, 69, 72, 74, 76, 79, 81, 84};
-//    int noteIndex = map(analogRead(A1), 0, 1023, 0, 15);
-//    Serial.println(noteIndex);
-//    pentatonicMidi[noteIndex];
-//    midiNoteOn(0, pentatonicMidi[noteIndex], 127);
-//    delay(200);
-//    midiNoteOff(0, pentatonicMidi[noteIndex], 127);
+//    for (int n = 0; n < numNotes; n++)
+//    {
+//
+//    }
 //  }
-//}
+
+
+}
+
+void setMatrixDensity(int a)
+{
+    for (int b = 0; b < numBeats; b++)
+  {
+    for (int n = 0; n < numNotes; n++)
+    {
+      noteMatrix[b][n] = !(random(20) < a);
+    }
+  }
+}
